2936906f54529f0df61dd44796042fe131842d01
Michael Joseph <michaeljoseph+github@gmail.com>
2013-08-21 16:00:14 +0200

    Read license and fix missing comma

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     name=changes.__name__,
     version=changes.__version__,
     description=changes.__doc__,
-    author=changes.__author__
+    author=changes.__author__,
     author_email=changes.__email__,
     url=changes.__url__,
     packages=[changes.__name__],
     install_requires=[
-        'docopt < 1.0.0', 
+        'docopt < 1.0.0',
         'semantic_version < 1.0.0'
     ],
     test_suite='nose.collector',
-    license=open('LICENSE'),
+    license=open('LICENSE').read(),
 )

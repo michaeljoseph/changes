34b0483491284adc19eaea68707a9cba616b4ebb
Michael Joseph <michaeljoseph+github@gmail.com>
2013-09-03 14:33:59 +0200

    Document `extract`

--- changes/cli.py
+++ changes/cli.py
@@ -18,9 +18,9 @@ def extract(dictionary, keys):
     """
     Extract only the specified keys from a dict
 
-    :param dictionary: 
-    :param keys: 
-    :return dict:
+    :param dictionary: source dictionary
+    :param keys: list of keys to extract
+    :return dict: extracted dictionary
     """
     return dict(
         (k, dictionary[k]) for k in keys if k in dictionary

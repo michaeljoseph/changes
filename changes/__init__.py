7beb4ee35bf0d3c810cffc99d8fcb6c2bf5019c7
Michael Joseph <michaeljoseph+github@gmail.com>
2013-11-15 22:26:30 +0200

    0.4.0

--- changes/__init__.py
+++ changes/__init__.py
@@ -1,6 +1,6 @@
 """Generates a github changelog, tags and uploads your python library"""
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __url__ = 'https://github.com/michaeljoseph/changes'
 __author__ = 'Michael Joseph'
 __email__ = 'michaeljoseph@gmail.com'

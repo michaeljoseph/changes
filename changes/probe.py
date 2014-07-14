12d674b29d31f007e6d7f41f87a17ed8a49ff930
Michael Joseph <michaeljoseph+github@gmail.com>
2014-07-14 13:22:07 +0200

    Premature exceptions

--- changes/probe.py
+++ changes/probe.py
@@ -3,7 +3,7 @@ from os.path import exists
 
 from fabric.api import local
 
-from changes import attributes, config, exceptions
+from changes import attributes, config
 
 log = logging.getLogger(__name__)
 

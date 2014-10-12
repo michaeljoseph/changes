94598bffaf0e442835774e4e684e9694c00723bd
Michael Joseph <michaeljoseph+github@gmail.com>
2014-10-12 11:41:57 +0200

    Explicitly package in the release command

--- changes/flow.py
+++ changes/flow.py
@@ -3,7 +3,7 @@ import logging
 from plumbum import local, CommandNotFound
 
 from changes.changelog import generate_changelog
-from changes.packaging import install_package, upload_package, install_from_pypi
+from changes.packaging import build_package, install_package, upload_package, install_from_pypi
 from changes.vcs import tag_and_push, commit_version_change
 from changes.verification import run_tests
 from changes.version import increment_version
@@ -20,6 +20,7 @@ def perform_release(context):
             generate_changelog(context)
         increment_version(context)
 
+        build_package(context)
         install_package(context)
         upload_package(context)
         install_from_pypi(context)

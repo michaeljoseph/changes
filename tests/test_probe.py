5c5c5c34db60b49c1f002f8f4e1da57557c31de2
Michael Joseph <michaeljoseph+github@gmail.com>
2014-12-08 11:56:03 +0200

    Update probe tests

--- tests/test_probe.py
+++ tests/test_probe.py
@@ -1,6 +1,3 @@
-from pytest import raises
-import shutil
-
 from changes import probe
 from . import context, setup, teardown
 
@@ -8,7 +5,14 @@ from . import context, setup, teardown
 def test_probe_project():
     assert probe.probe_project(context)
 
-def test_probe_with_alt_requirements():
-    with raises(Exception):
-        shutil.rmtree(context.requirements)
-        probe.probe_project(context)
+
+def test_has_binary():
+    assert probe.has_binary('git')
+
+
+def test_has_no_binary():
+    assert not probe.has_binary('foo')
+
+
+def test_has_test_runner():
+    assert probe.has_test_runner()

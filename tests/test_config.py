aa985031f9134f00f1c6ff79154fcac03e88a7a5
Michael Joseph <michaeljoseph+github@gmail.com>
2014-12-09 12:17:01 +0200

    Test config

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,7 @@ from . import context, setup, teardown  # noqa
 
 def test_no_config():
     assert not exists('test_app/.changes')
-    assert config.project_config(context) == config.DEFAULTS
+    assert config.project_config('test_app') == config.DEFAULTS
     assert exists('test_app/.changes')
 
 
@@ -16,13 +16,19 @@ def test_existing_config():
     existing_config = 'foo: bar\nbaz: buzz\n'
     with click.open_file('test_app/.changes', 'w') as f:
         f.write(existing_config)
-    assert config.project_config(context) == {'foo': 'bar', 'baz': 'buzz'}
+    assert config.project_config('test_app') == {'foo': 'bar', 'baz': 'buzz'}
 
 
-def test_malformed_config_returns_none():
+def test_malformed_config_returns_dict():
     with click.open_file('test_app/.changes', 'w') as f:
         f.write('something\n\n-another thing\n')
-        assert config.project_config(context) == {}
+        assert config.project_config('test_app') == {}
+
+
+def test_store_settings():
+    config.store_settings('test_app', dict(foo='bar'))
+    assert exists('test_app/.changes')
+    assert config.project_config('test_app') == dict(foo='bar')
 
 
 def test_parsed_repo_url():

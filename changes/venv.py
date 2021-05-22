8b13b5811e1d6982cedf115c05e7cf0e7f2162a8
Michael Joseph <michaeljoseph+github@gmail.com>
2021-05-23 00:42:22 +0200

    Update the build

--- changes/venv.py
+++ changes/venv.py
@@ -8,7 +8,7 @@ from plumbum.cmd import virtualenv
 def create_venv(tmp_dir=None):
     if not tmp_dir:
         tmp_dir = tempfile.mkdtemp()
-    virtualenv('--no-site-packages', tmp_dir)
+    virtualenv(tmp_dir)
     return tmp_dir
 
 
